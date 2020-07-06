b83f7d5e9d0059686142dbb0e07b84c5914a6b9b
pradeepgusain5 <pradeepgusain5@gmail.com>
2020-07-06 18:12:46 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const hbs = require('hbs')
 const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 //Defined lines for express config
 const publicDirectoryPath = path.join(__dirname,'../public')
@@ -97,6 +98,7 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
 })
+
